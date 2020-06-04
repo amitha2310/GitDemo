9d00438eb008c6d4f44b237125b2b9bd98584039
navee <amitha2310@gmail.com>
2020-06-04 14:29:35 -0700

    This is my first commit

---
+++ src/test/java/FV_Project/QlikStyling/RestAPITest.java
@@ -0,0 +1,13 @@
+package FV_Project.QlikStyling;
+
+import org.testng.annotations.Test;
+
+public class RestAPITest {
+	
+	@Test
+	public void RAPIKey()
+	{
+		System.out.println("Rest API Automation");
+	}
+
+}
